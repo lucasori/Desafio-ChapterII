e4ef91449efa8e0674548886e927629432e03e99
lucasori <lucasorihashi11@gmail.com>
2022-02-17 19:10:51 -0300

    Desafio Chapter II

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+
+    const list = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+
+    return response.json(list);
+  }
+}
+
+export { ListAllUsersController };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.admin = true;
+    user.updated_at = new Date();
+
+    return user;
+  }
+}
+
+export { TurnUserAdminUseCase };
